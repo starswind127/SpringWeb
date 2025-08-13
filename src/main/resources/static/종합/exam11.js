7d5e73a0d01f6c512d3d1941a4668607df245ec0
starwind127 <kanzen127@naver.com>
2025-08-13 18:27:19 +0900

    exam11 조회, 수정,삭제 까지

--- src/main/resources/static/종합/exam11.js
+++ src/main/resources/static/종합/exam11.js
@@ -9,7 +9,41 @@ const boardWrite = ( ) => {
         body : JSON.stringify( data )
     }
     fetch("/board" , option )
-    .then( response => response.json() )
-    .then( data => {console.log(data)})
-    .catch( error => {console.log(error)})
+        .then( response => response.json() )
+        .then( data => {console.log(data)})
+        .catch( error => {console.log(error)})
+}
+const boardPrint = () => {
+    console.log( "boardPrint() exe" )
+    let option = { method : "GET" }
+    fetch( "/board" , option )
+        .then( response => response.json() )
+        .then( data => { console.log(data)})
+        .catch( error => {console.log(error)})
+
+   
+}
+
+const boardDelete = () => {
+    console.log("boardDelete() exe")
+    let bno = 3;
+    let option = { method : "DELETE"}
+    fetch(`/board?bno=${bno}` , option)
+        .then( response => response.json() )
+        .then( data => { console.log(data)})
+        .catch( error => {console.log(error)})
+}
+
+const boardUpdate = () => {
+    console.log("boardUpdate() exe")
+    let data = { "bno" : 37 , "bcontent" : "JS에서수정"}
+    let option = {
+        method : "PUT",
+        headers : {"Content-Type" : "application/json"},
+        body : JSON.stringify( data )
+    }
+    fetch("/board" , option )
+        .then( response => response.json() )
+        .then( data => {console.log(data)})
+        .catch( error => {console.log(error)}) 
 }
